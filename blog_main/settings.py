b4d1f24e4156596978373acabdd4069365624b2f
Yasika <yasikamohanraj@gmail.com>
2025-12-13 22:29:06 +0530

    blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ STATICFILES_DIRS = [
     'blog_main/static',
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MIDIA_ROOT=BASE_DIR /'media'
